b37fe577b22c87864602df78a60dafe529780f7f
shubhi <shubhijain0606@gmail.com>
2019-07-12 11:35:14 +0530

    submitted task 6 i.e. bean post processor for spring xml demo

--- src/main/java/com/stackroute/domain/Demo.java
+++ src/main/java/com/stackroute/domain/Demo.java
@@ -1,10 +1,6 @@
 package com.stackroute.domain;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.ClassPathResource;
 
 
 public class Demo {
@@ -15,7 +11,10 @@ public class Demo {
         ConfigurableApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
 
         // create object of BeanLifeCycleDemoBean
-        BeanLifecycleDemoBean beanLifecycleDemoBean= context.getBean("BeanLifeCycleDemoBean",BeanLifecycleDemoBean.class);
+        BeanLifecycleDemoBean beanLifecycleDemoBean= context.getBean("BeanLifecycleDemoBean",BeanLifecycleDemoBean.class);
+
+        // create object of BeanPostProcessorDemoBean
+        BeanPostProcessorDemoBean beanPostProcessorDemoBean= context.getBean("BeanPostProcessorDemoBean",BeanPostProcessorDemoBean.class);
 
         // close
         context.close();
