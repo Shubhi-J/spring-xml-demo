432f9b401bb4f81ba2b3f6659763a72aacac4092
shubhi <shubhijain0606@gmail.com>
2019-07-12 11:07:09 +0530

    submitted task 5 i.e. bean lifecycle for spring xml demo

--- src/main/java/com/stackroute/domain/Demo.java
+++ src/main/java/com/stackroute/domain/Demo.java
@@ -2,6 +2,7 @@ package com.stackroute.domain;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 
@@ -10,18 +11,13 @@ public class Demo {
 
     public static void main(String[] args) {
 
-        // using application context
-        ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
+        // using configurable application context
+        ConfigurableApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
 
-         // create object of Movie1 bean
-           Movie movie1=context.getBean("Movie1", Movie.class);
-          // movie1.displayActor();
+        // create object of BeanLifeCycleDemoBean
+        BeanLifecycleDemoBean beanLifecycleDemoBean= context.getBean("BeanLifeCycleDemoBean",BeanLifecycleDemoBean.class);
 
-        // using BeanFactory
-           BeanFactory factory=new XmlBeanFactory(new ClassPathResource("beans.xml"));
-            Movie movie2 =factory.getBean("Movie1", Movie.class);
-           // movie2.displayActor();
-}
-
-
+        // close
+        context.close();
+    }
 }
